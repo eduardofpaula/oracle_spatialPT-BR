4653c3662521f78ba7ae5981a3ebd75aaca90ce8
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-04-19 22:01:59 -0300

    init: init project

---
+++ oracleDatabase/src/main/java/oracle/spatial/oracleDatabase/OracleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package oracle.spatial.oracleDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDatabaseApplication.class, args);
+	}
+
+}
---
+++ oracleDatabase/src/test/java/oracle/spatial/oracleDatabase/OracleDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package oracle.spatial.oracleDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
