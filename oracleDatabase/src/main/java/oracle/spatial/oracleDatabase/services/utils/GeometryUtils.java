797da241ecc88bc1de1d7affcde9b7069bec48f4
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-05-04 18:40:50 -0300

    fix: imports correct geoms

--- oracleDatabase/src/main/java/oracle/spatial/oracleDatabase/services/utils/GeometryUtils.java
+++ oracleDatabase/src/main/java/oracle/spatial/oracleDatabase/services/utils/GeometryUtils.java
@@ -1,20 +1,22 @@
 package oracle.spatial.oracleDatabase.services.utils;
 
-import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
 
 @Component
 public class GeometryUtils {
 
-    public static Point createPoint(double latitude, double longitude){
-        if(latitude == 0 || longitude == 0){
+    public static Point createPoint(double latitude, double longitude) {
+        if (latitude == 0 || longitude == 0) {
             throw new IllegalArgumentException("Latitude ou Longitude vazios");
         }
 
         GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
-        Coordinate pointCoordinates = new Coordinate(longitude,latitude);
+        Coordinate pointCoordinates = new Coordinate(longitude, latitude);
         return geometryFactory.createPoint(pointCoordinates);
     }
+
 }
